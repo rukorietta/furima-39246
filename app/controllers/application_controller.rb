81b88be303201e7741e7d4393fe089712cac06b1
rukorietta <130912664+rukorietta@users.noreply.github.com>
2023-05-09 17:10:39 +0900

    Revert "Basic認証"
    This reverts commit 073b02db7a5f483c159441e567040bfcd595d0c7.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  config.middleware.use Rack::Auth::Basic do |username, password|
-    username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-  end
 end
