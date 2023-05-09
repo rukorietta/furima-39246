073b02db7a5f483c159441e567040bfcd595d0c7
rukorietta <130912664+rukorietta@users.noreply.github.com>
2023-05-09 17:03:37 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  config.middleware.use Rack::Auth::Basic do |username, password|
+    username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+  end
 end
