e8ff47941e35b5b02ae6eeaadfc31866ea7485b5
rukorietta <130912664+rukorietta@users.noreply.github.com>
2023-05-18 19:27:37 +0900

    コントローラー

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,59 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy] # create アクションもログイン状態でのアクセスを制限する
+  before_action :set_item, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @items = Item.order(created_at: :desc)
+  end
+
+  def new
+    # ログイン状態の場合は商品出品ページを表示
+    @item = Item.new
+  end
+
+  def create
+    @item = current_user.items.build(item_params) # ログインユーザーに紐づく商品を作成
+
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました。'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: "商品情報が更新されました。"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+     if @item.user != current_user
+       redirect_to root_path, alert: "他のユーザーの商品は編集できません。"
+     end
+  end
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    if current_user == @item.user
+      @item.destroy
+      redirect_to root_path, notice: '商品を削除しました。'
+    else
+      redirect_to root_path, alert: '他のユーザーの商品は削除できません。'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_day_id, :delivery_fee_id, :prefecture_id, :price, :image)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,14 @@ ActiveRecord::Schema.define(version: 2023_05_15_074534) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "price"
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "condition_id", null: false
+    t.integer "category_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "delivery_day_id", null: false
+    t.integer "prefecture_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
